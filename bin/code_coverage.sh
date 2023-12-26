1701afc2b641fd2f0c52102d70ad3c6ce87056d9
Philipp Limmer <philipp.limmer@web.de>
2023-12-26 15:48:25 +0100

    [TASK] Add basic test setup

---
+++ bin/code_coverage.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+XDEBUG_MODE=coverage ./vendor/bin/phpunit -c phpunit.xml.dist --coverage-html code-coverage-report
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LimLabs\KafkaBundle\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testStuff(): void
+    {
+        $this->assertTrue(true);
+    }
+}
